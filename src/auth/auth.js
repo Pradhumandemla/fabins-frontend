6f836e3c72a326e7cbbd5160ca40ae6fe847765d
Pradhuman Demla <Pradhumnd33@gmail.com>
2022-11-23 01:24:53 +0530

    initials
    Signed-off-by: Pradhuman Demla <Pradhumnd33@gmail.com>

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -4,15 +4,16 @@ import * as redux from "../redux/Auth/authSlice";
 export const login = async (dispatch, newUser) => {
     try {
         let res = await axios.post(process.env.REACT_APP_BACKEND + "/auth/login", { email: newUser.email, password: newUser.password })
-        if (res.status !== 200) {
-            console.log("error " + res.status);
-            localStorage.removeItem("token")
-            // error
-            return res
-        }
-        let { token } = res.data
-        localStorage.setItem("token", token)
-        dispatch(redux.login(token))
+        // if (res.status !== 200) {
+        //     console.log("error " + res.status);
+        //     localStorage.removeItem("token")
+        //     // error
+        //     return res
+        // }
+        // let { token } = res.data
+        // localStorage.setItem("token", token)
+        // dispatch(redux.login(token))
+        console.log(res);
         dispatch(redux.login("ashdajhs"))
     } catch (error) {
         console.log(error);
