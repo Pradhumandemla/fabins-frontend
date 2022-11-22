741f3f14caeb7592a2ee44fc045b54bd9182ecd8
Pradhuman Demla <Pradhumnd33@gmail.com>
2022-11-23 00:11:02 +0530

    initials
    Signed-off-by: Pradhuman Demla <Pradhumnd33@gmail.com>

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -3,36 +3,36 @@ import * as redux from "../redux/Auth/authSlice";
 
 export const login = async (dispatch, newUser) => {
     try {
-        // let res = await axios.post(process.env.REACT_APP_BACKEND + "/auth/login", { email: newUser.email, password: newUser.password })
-        // if (res.status !== 200) {
-        //     console.log("error " + res.status);
-        //     localStorage.removeItem("token")
-        //     // error
-        //     return res
-        // }
-        // let { token } = res.data
-        // localStorage.setItem("token", token)
-        // dispatch(redux.login(token))
+        let res = await axios.post(process.env.REACT_APP_BACKEND + "/auth/login", { email: newUser.email, password: newUser.password })
+        if (res.status !== 200) {
+            console.log("error " + res.status);
+            localStorage.removeItem("token")
+            // error
+            return res
+        }
+        let { token } = res.data
+        localStorage.setItem("token", token)
+        dispatch(redux.login(token))
     } catch (error) {
-        // console.log(error);
+        console.log(error);
         return
     }
 }
 
 export const register = async (dispatch, newUser) => {
     try {
-        // let res = await axios.post(process.env.REACT_APP_BACKEND + "/auth/register", { ...newUser })
-        // if (res.status !== 200) {
-        //     console.log("error " + res.status);
-        //     localStorage.removeItem("token")
-        //     // error
-        //     return res
-        // }
-        // let { token } = res.data
-        // localStorage.setItem("token", token)
-        // dispatch(redux.register(token))
+        let res = await axios.post(process.env.REACT_APP_BACKEND + "/auth/register", { ...newUser })
+        if (res.status !== 200) {
+            console.log("error " + res.status);
+            localStorage.removeItem("token")
+            // error
+            return res
+        }
+        let { token } = res.data
+        localStorage.setItem("token", token)
+        dispatch(redux.register(token))
     } catch (error) {
-        // console.log(error);
+        console.log(error);
         return
     }
 }
